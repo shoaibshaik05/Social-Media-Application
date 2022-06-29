f5d09dcd224cd8f5753f6eda9f87070227e07e13
shoaibshaik05 <shoaibmahaboobshaik1999@gmail.com>
2022-06-29 18:26:29 +0530

    First Commit: create the directory structure and setuo express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`There is an error in the server: ${err}`);
+    }
+    console.log(`Server is running on the port: ${port}`);
+})
