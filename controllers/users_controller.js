fdae6726ac50a87701533b15b3f95bf89025cf92
shoaibshaik05 <shoaibmahaboobshaik1999@gmail.com>
2022-06-30 22:03:02 +0530

    add passport authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,6 +8,11 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
@@ -16,6 +21,11 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
@@ -46,5 +56,14 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    // TODO later
+    return res.redirect('/');
 }
+
+//sign out and clear the session
+module.exports.destroySession = function(req, res){
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+}
+
--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
+//used for session cookies encription
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+//to store the session and prevent from restarting
+const MongoStore = require('connect-mongo')(session);
+
+//scss middleware to convert scss to css
+const sassMiddleware = require('node-sass-middleware');
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
+
 
 //middle wares
 app.use(express.urlencoded());
@@ -21,13 +39,45 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
+//setup the view engine 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+//this is used to keep the session established for some time
+//using mongo store to keep session live by storing in db
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongoDb setup ok');
+        }
+    )
+}));
+
+
+
+//passport js is used for authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
 
 // use express router
 app.use('/', require('./routes'));
 
-//setup the view engine 
-app.set('view engine', 'ejs');
-app.set('views', './views');
 
 
 app.listen(port, function(err){
